4a608942a8749f489acd540dd76cd8547394b14d
Nikita Khristinin <nkhristinin@gmail.com>
2024-01-22 16:46:02 +0100

    Entity store init

---
+++ commands/entity-store.mjs
@@ -0,0 +1,186 @@
+import { faker } from "@faker-js/faker";
+import { getEsClient, indexCheck } from "./utils/index.mjs";
+import { chunk } from "lodash-es";
+import moment from "moment";
+import auditbeatMappings from "../mappings/auditbeat.json" assert { type: "json" };
+import { assignAssetCriticality, enableRiskScore, createRule } from "./api.mjs";
+
+let client = getEsClient();
+let EVENT_INDEX_NAME = "auditbeat-8.12.0-2024.01.18-000001";
+
+const ASSET_CRITICALITY = [
+  "very_important",
+  "not_important",
+  "normal",
+  "important",
+  "unknown",
+];
+
+export const createRandomUser = () => {
+  return {
+    name: `User-${faker.internet.userName()}`,
+    assetCriticality: faker.helpers.arrayElement(ASSET_CRITICALITY),
+  };
+};
+
+export const createRandomHost = () => {
+  return {
+    name: `Host-${faker.internet.domainName()}`,
+    assetCriticality: faker.helpers.arrayElement(ASSET_CRITICALITY),
+  };
+};
+
+export const createFactoryRandomEventForHost = (name) => () => {
+  return {
+    "@timestamp": moment().format("yyyy-MM-DDTHH:mm:ss.SSSSSSZ"),
+    message: `Host ${faker.hacker.phrase()}`,
+    service: {
+      type: "system",
+    },
+    host: {
+      name,
+      id: faker.number.int({ max: 10000 }),
+      ip: faker.internet.ip(),
+      mac: faker.internet.mac(),
+      os: {
+        name: faker.helpers.arrayElement(["Windows", "Linux", "MacOS"]),
+      },
+    },
+  };
+};
+
+export const createFactoryRandomEventForUser = (name) => () => {
+  return {
+    "@timestamp": moment().format("yyyy-MM-DDTHH:mm:ss.SSSSSSZ"),
+    message: `User ${faker.hacker.phrase()}`,
+    service: {
+      type: "system",
+    },
+    user: {
+      name,
+      id: faker.number.int({ max: 10000 }),
+      entity_id: faker.string.nanoid(),
+    },
+  };
+};
+
+const ingestEvents = async (events) => {
+
+  await indexCheck(EVENT_INDEX_NAME, auditbeatMappings);
+
+  let chunks = chunk(events, 10000);
+
+  for (let chunk of chunks) {
+    try {
+      // Make bulk request
+      let ingestRequest = chunk.reduce((acc, event) => {
+        acc.push({ index: { _index: EVENT_INDEX_NAME } });
+        acc.push(event);
+        return acc;
+      }, []);
+      const result = await client.bulk({ body: ingestRequest, refresh: true });
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  }
+};
+
+export const generateEvents = (entities, createEventFactory) => {
+  const eventsPerEntity = 10;
+  return entities.reduce((acc, entity) => {
+    const events = faker.helpers.multiple(createEventFactory(entity.name), {
+      count: eventsPerEntity,
+    });
+    acc.push(...events);
+    return acc;
+  }, []);
+};
+
+const assignAssetCriticalityToEntities = async (entities, field) => {
+  for (const entity of entities) {
+    const { name, assetCriticality } = entity;
+    if (assetCriticality === "unknown") return;
+    await assignAssetCriticality({
+      id_field: field,
+      id_value: name,
+      criticality_level: assetCriticality,
+    });
+  }
+};
+
+/**
+ * Generate entities first
+ * Then Generate events, assign asset criticality, create rule and enable risk engine
+ * @param {*} param0
+ */
+export const generateEntityStore = async ({ users = 10, hosts = 10 }) => {
+  try {
+    const generatedUsers = faker.helpers.multiple(createRandomUser, {
+      count: users,
+    });
+
+    const generatedHosts = faker.helpers.multiple(createRandomHost, {
+      count: hosts,
+    });
+
+    let eventsForUsers = generateEvents(
+      generatedUsers,
+      createFactoryRandomEventForUser
+    );
+    let eventsForHosts = generateEvents(
+      generatedHosts,
+      createFactoryRandomEventForHost
+    );
+
+    await ingestEvents(eventsForUsers);
+    console.log("Users events ingested");
+    await ingestEvents(eventsForHosts);
+    console.log("Hosts events ingested");
+    console.log(generatedUsers)
+
+    await assignAssetCriticalityToEntities(generatedUsers, "user.name");
+    console.log("Assigned asset criticality to users");
+    await assignAssetCriticalityToEntities(generatedHosts, "host.name");
+    console.log("Assigned asset criticality to hosts");
+
+    await enableRiskScore();
+    console.log("Risk score enabled");
+
+    await createRule();
+    console.log("Rule created");
+
+    console.log("Finished generating entity store");
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+export const cleanEntityStore = async () => {
+  console.log("Deleting all entity-store data...");
+  try {
+    console.log("Deleted all events");
+    await client.deleteByQuery({
+      index: EVENT_INDEX_NAME,
+      refresh: true,
+      body: {
+        query: {
+          match_all: {},
+        },
+      },
+    });
+
+    console.log("Deleted asset criticality");
+    await client.deleteByQuery({
+      index: '.asset-criticality.asset-criticality-default',
+      refresh: true,
+      body: {
+        query: {
+          match_all: {},
+        },
+      },
+    });
+  } catch (error) {
+    console.log("Failed to clean data");
+    console.log(error);
+  }
+};
--- index.mjs
+++ index.mjs
@@ -1,50 +1,72 @@
 #! /usr/bin/env node
-import { program } from 'commander';
-import { generateAlerts, deleteAllAlerts, deleteAllEvents, generateGraph, generateEvents } from './commands/documents.mjs';
-import { fetchRiskScore } from './commands/risk-score.mjs';
-import config from './config.json'  assert { type: "json" };
+import { program } from "commander";
+import {
+  generateAlerts,
+  deleteAllAlerts,
+  deleteAllEvents,
+  generateGraph,
+  generateEvents,
+} from "./commands/documents.mjs";
+import { fetchRiskScore } from "./commands/api.mjs";
+import { cleanEntityStore, generateEntityStore } from "./commands/entity-store.mjs";
+import config from "./config.json" assert { type: "json" };
 
-const withValidation  = (fn) => (...args) => {
-  if(!config.elastic.node || !config.elastic.apiKey) {
-    console.log('Please provide elastic node and apiKey in config.json')
-    return;
-}
-return fn(...args)
-}
+const withValidation =
+  (fn) =>
+  (...args) => {
+    if(!config.elastic.node) {
+      return console.log("Please provide elastic node in config.json");
+    }
+    const hasApiKey = config.elastic.apiKey;
+    const hasPassword = config.elastic.username && config.elastic.password;
+    if (!hasApiKey && !hasPassword) {
+      console.log("Please provide elastic apiKey or username/password in config.json");
+      return;
+    }
+    return fn(...args);
+  };
 
 program
-  .command('generate-alerts')
-  .argument('<n>', 'integer argument', parseInt)
-  .description('Generate fake alerts')
-  .action(withValidation(generateAlerts))
-
-
-  program
-  .command('generate-events')
-  .argument('<n>', 'integer argument', parseInt)
-  .description('Generate events')
-  .action(withValidation(generateEvents))
-
+  .command("generate-alerts")
+  .argument("<n>", "integer argument", parseInt)
+  .description("Generate fake alerts")
+  .action(withValidation(generateAlerts));
 
 program
-  .command('generate-graph')
+  .command("generate-events")
+  .argument("<n>", "integer argument", parseInt)
+  .description("Generate events")
+  .action(withValidation(generateEvents));
+
+program
+  .command("generate-graph")
   // .argument('<n>', 'integer argument', parseInt)
-  .description('Generate fake graph')
-  .action(withValidation(generateGraph))
+  .description("Generate fake graph")
+  .action(withValidation(generateGraph));
 
 program
-  .command('delete-alerts')
-  .description('Delete all alerts')
-  .action(withValidation(deleteAllAlerts))
-
-  program
-  .command('delete-events')
-  .description('Delete all events')
-  .action(withValidation(deleteAllEvents))
+  .command("delete-alerts")
+  .description("Delete all alerts")
+  .action(withValidation(deleteAllAlerts));
 
 program
-  .command('test-risk-score')
-  .description('Test risk score API')
-  .action(withValidation(fetchRiskScore))
+  .command("delete-events")
+  .description("Delete all events")
+  .action(withValidation(deleteAllEvents));
+
+program
+  .command("test-risk-score")
+  .description("Test risk score API")
+  .action(withValidation(fetchRiskScore));
+
+program
+  .command("entity-store")
+  .description("Generate entity store")
+  .action(withValidation(generateEntityStore));
+
+program
+  .command("clean-entity-store")
+  .description("Generate entity store")
+  .action(withValidation(cleanEntityStore));
 
 program.parse();
