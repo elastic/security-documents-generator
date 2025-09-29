012677e06758084cc2c382a9ab2ae286d0c3404c
CAWilson94 <charlotte.wilson@elastic.co>
2025-09-29 12:33:22 +0100

    Add fullsync events data generation for integrations sync [privmon]

--- src/commands/privileged_user_monitoring/sample_documents.ts
+++ src/commands/privileged_user_monitoring/sample_documents.ts
@@ -1,4 +1,7 @@
-import { OktaSampleUser } from '../utils/okta_utils';
+import {
+  FullSyncEntityEventDoc,
+  OktaSampleUser,
+} from '../utils/integrations_sync_utils';
 import {
   userNameAsEmail,
   userNameWhitespaceRemoved,
@@ -578,6 +581,32 @@ export const OKTA_USERS_SAMPLE_DOCUMENT = (
       },
     },
   };
+};
+
+export const OKTA_SAMPLE_ENTITY_DOCUMENTS = (datasetName: string) => {
+  // event action should be start or completed
+  return [
+    {
+      event: {
+        agent_id_status: 'verified',
+        ingested: '2025-06-08T09:21:53Z',
+        kind: 'asset',
+        start: '2025-06-08T09:21:42.931Z',
+        action: 'started',
+        dataset: datasetName,
+      },
+    },
+    {
+      event: {
+        agent_id_status: 'verified',
+        ingested: '2025-06-08T09:16:44Z',
+        kind: 'asset',
+        action: 'completed',
+        end: '2025-06-08T09:16:42.927Z',
+        dataset: datasetName,
+      },
+    },
+  ] satisfies FullSyncEntityEventDoc[];
 };
 
 export const GRANTED_RIGHTS_OKTA_SAMPLE_DOCUMENT = (
